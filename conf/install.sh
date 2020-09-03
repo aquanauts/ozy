8e8185f4c0e5b702e04e3a0df0157c986c85de59
Matt Godbolt <matthew.godbolt@aquatic.com>
2020-09-03 11:29:12 -0500

    Fix up example config link

--- conf/install.sh
+++ conf/install.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 
 echo Installing ozy pointing at the ozy sample team configuration on GitHub...
 
-OZY_CONFIG_URL=https://raw.githubusercontent.com/aquanauts/ozy/master/conf/sample-team-conf.yaml
+OZY_CONFIG_URL=https://raw.githubusercontent.com/aquanauts/ozy/main/conf/sample-team-conf.yaml
 OZY_OS=linux
 if [[ "$(uname)" == "Darwin" ]]; then OZY_OS=osx; fi
 OZY_VERSION=$(curl -sL ${OZY_CONFIG_URL} | grep 'ozy_version:' | awk '{print $2}')
