bdd392d15afe111a884c159c0afcd23d157d43e4
kpweiler <kpweiler@gmail.com>
2021-05-12 13:22:19 -0500

    added conda_bin config (#19)

--- ozy/installers/conda.py
+++ ozy/installers/conda.py
@@ -9,7 +9,7 @@ _LOGGER = logging.getLogger(__name__)
 
 class CondaInstaller(Installer):
     def __init__(self, name, config):
-        super().__init__(name, config, 'package', 'version', channels=[])
+        super().__init__(name, config, 'package', 'version', channels=[], conda_bin="conda")
 
     def __str__(self):
         return f'conda app installer for {self.config("package")}'
@@ -19,7 +19,8 @@ class CondaInstaller(Installer):
         channels = []
         for channel in self.config('channels'):
             channels += ['-c', channel]
-        args = (['conda', 'create', '-y'] + channels
+        conda_bin=self.config("conda_bin")
+        args = ([conda_bin, 'create', '-y'] + channels
                 + ['-p', to_dir, f'{self.config("package")}={self.config("version")}'])
         _LOGGER.debug("Executing %s", " ".join(args))
         check_call(args)
